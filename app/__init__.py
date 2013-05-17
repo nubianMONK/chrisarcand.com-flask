49b55c64fbe402604c42201e2d3aaa96b9cf0f67
ChrisArcand <chris@chrisarcand.com>
2013-05-16 22:32:04 -0500

    Can't use . in mongodb name, derp

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "chrisarcand.com"}
+app.config["MONGODB_SETTINGS"] = {'DB': "chrisarcand-flask-site"}
 app.config["SECRET_KEY"] = "thisisasupersecretpassword1!"
 
 db = MongoEngine(app)
