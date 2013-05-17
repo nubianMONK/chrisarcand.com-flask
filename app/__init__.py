1531decfadfddca4f24032a12c40732c289c79ff
ChrisArcand <chris@chrisarcand.com>
2013-05-16 22:04:35 -0500

    Added post and comment models

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "chrisarcand.com"}
+app.config["SECRET_KEY"] = "thisisasupersecretpassword1!"
+
+db = MongoEngine(app)
 
 if __name__ == '__main__':
     app.run()
