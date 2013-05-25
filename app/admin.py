f979147dd7f138a1b53a71c4e81016c4e12ab12d
Chris Arcand <chris@chrisarcand.com>
2013-05-25 11:35:37 -0500

    Removed different post types, lets use Markdown instead

--- app/admin.py
+++ app/admin.py
@@ -4,17 +4,7 @@ from flask.views import MethodView
 from flask.ext.mongoengine.wtf import model_form
 
 from app.auth import requires_auth
-from app.models import Post, BlogPost, Video, Image, Quote, Comment
-
-# Flask Blueprints are a little confusing reading Armin's documentation (Sorry, Armin)
-# Basically, blueprints allow abstraction of URLs and site operations by creating 
-# app-like/class-like functionality. For example, you can make a 'main_views.py' and
-# an 'admin_views.py' which specify their own blueprints with their own index()'s. The
-# blueprints all you to have the two indexes, which you can call by getting the URL with
-# url_for(admin.index) and url_for(main.index). You can also specify url_prefix which
-# allows you to treat all admin-related functions in a general sense (/index) but have
-# the /admin/index/ added automatically. Blueprints make large, complex apps easier to
-# manage. Neat. For more: http://flask.pocoo.org/docs/blueprints/
+from app.models import Post, Comment
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
@@ -31,35 +21,25 @@ class List(MethodView):
 class Detail(MethodView):
 
     decorators = [requires_auth]
-    # Map post types to models
-    class_map = {
-        'post': BlogPost,
-        'video': Video,
-        'image': Image,
-        'quote': Quote,
-    }
 
     def get_context(self, slug=None):
+        form_cls = model_form(Post, exclude=('created_at', 'comments'))
 
         if slug:
             post = Post.objects.get_or_404(slug=slug)
-            # Handle old posts types as well
-            cls = post.__class__ if post.__class__ != Post else BlogPost
-            form_cls = model_form(cls,  exclude=('created_at', 'comments'))
             if request.method == 'POST':
                 form = form_cls(request.form, inital=post._data)
             else:
                 form = form_cls(obj=post)
         else:
-            # Determine which post type we need
-            cls = self.class_map.get(request.args.get('type', 'post'))
-            post = cls()
-            form_cls = model_form(cls,  exclude=('created_at', 'comments'))
+            post = Post()
             form = form_cls(request.form)
+
         context = {
             "post": post,
             "form": form,
             "create": slug is None
+            # Confusing shorthand. Above line is a boolean. Create==True if slug is None
         }
         return context
 
@@ -75,16 +55,15 @@ class Detail(MethodView):
             post = context.get('post')
             form.populate_obj(post)
             post.save()
+            return redirect(url_for('admin.index'))
         elif form.validate() and request.form['action'] == 'delete':
             post = context.get('post')
             post.delete()
-
             return redirect(url_for('admin.index'))
         return render_template('admin/detail.html', **context)
 
 
 # Register the urls
-# http://flask.pocoo.org/docs/api/#url-route-registrations
 admin.add_url_rule('/admin/', view_func=List.as_view('index'))
 admin.add_url_rule('/admin/create/', defaults={'slug': None}, view_func=Detail.as_view('create'))
 admin.add_url_rule('/admin/<slug>/', view_func=Detail.as_view('edit'))
