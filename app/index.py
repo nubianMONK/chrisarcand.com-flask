ab71e6699b177e4ca9e6adc64c161fe4131e1d7e
Chris Arcand <chris@chrisarcand.com>
2013-05-26 22:36:28 -0500

    Added content, all templates routed.

--- app/index.py
+++ app/index.py
@@ -4,4 +4,20 @@ index = Blueprint('index', __name__, template_folder='templates')
 
 @index.route("/")
 def home():
-	return render_template('base.html')
+	return render_template('content/index.html')
+
+@index.route("/about")
+def about():
+	return render_template('content/about.html')
+
+@index.route("/projects")
+def projects():
+	return render_template('content/projects.html')
+
+@index.route("/resume")
+def resume():
+	return render_template('content/resume.html')
+
+@index.route("/contact")
+def contact():
+	return render_template('content/contact.html')
