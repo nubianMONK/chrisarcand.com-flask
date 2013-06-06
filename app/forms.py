9d16d551aa28929d30da7882d6553824f4db4770
Chris Arcand <chris@chrisarcand.com>
2013-06-05 22:29:01 -0500

    Contact form, frontend, fixed messed up admin console from eons ago...

--- app/forms.py
+++ app/forms.py
@@ -4,4 +4,5 @@ from wtforms import TextField, TextAreaField, RadioField
 class ContactForm(Form):
     name = TextField('Name', [validators.DataRequired()])
     email = TextField('Email Address', [validators.DataRequired(), validators.Email()])
+    phone = TextField('Phone number (optional)', [validators.optional()])
     body = TextAreaField('Message', [validators.DataRequired()])
--- app/index.py
+++ app/index.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 from app.forms import ContactForm
+from app.models import Post
+
+from smtplib import SMTP_SSL as SMTP 
+from email.mime.text import MIMEText
 
 index = Blueprint('index', __name__, template_folder='templates')
 
 @index.route("/")
 def home():
-	return render_template('content/index.html')
+    posts = Post.objects.all()
+    return render_template('content/index.html', posts=posts)
 
 @index.route("/about")
 def about():
@@ -24,5 +29,18 @@ def resume():
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
-        return redirect(url_for("index.home"))
+    	name = form.name.data
+    	email = form.email.data
+    	phone = form.phone.data
+    	msg = MIMEText(form.body.data)
+
+    	msg['Subject'] = 'New message from ChrisArcand.com'
+    	msg['From'] = email
+    	# I should totally be using an external config file here, I know. Too lazy, for now...
+    	msg['To'] = 'chris@chrisarcand.com'
+
+    	#SMTP SENDER CODE HERE, WILL BE SPECIFIC TO VPS
+    	# Make sure you check to make sure the message sent successfully, else warn the user it failed
+
+        return render_template("content/contact.html", submitted=True)
     return render_template("content/contact.html", form=form)
